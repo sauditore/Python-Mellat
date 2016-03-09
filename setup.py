6c2e910b83914745e68b84226824c86f83a3a0fb
Saeed Auditore <sauditore@users.noreply.github.com>
2016-03-09 19:57:54 +0330

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='PyMellat',
+    version='1.0.1',
+    packages=['PyMellat'],
+    url='http://sauditore.github.io/Python-Mellat.',
+    license='Apache',
+    author='Saeed Auditore',
+    author_email='saeed.auditore@gmail.com',
+    description='Bank Mellat Python API'
+)
+
